ded5a98bfd1329f6a81da9eb36a49aa03306168f
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2023-07-30 10:38:17 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,23 @@
+// create web server
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        })
+    }
+    if (req.url === '/api/users') {
+        const users = [
+            { name: 'Bob Smith', age: 40 },
+            { name: 'John Doe', age: 30 }
+        ];
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(users));
+    }
+    // Build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    console.log(filePath);
